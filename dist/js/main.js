b9628b1682c4c353994f5d6275ca66eab8db644e
e.plougoulm <e.plougoulm@groupeonepoint>
2021-11-08 19:45:07 +0100

    ledger: add createCard function

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,3 +2,54 @@
 
 var message = "Write your JavaScript in src/main.js";
 console.log(message);
+
+function fetchList() {
+  fetch("https://jsonplaceholder.typicode.com/posts").then(function (response) {
+    return response.json();
+  }).then(function (res) {
+    console.log(res);
+    createCard(res[0]);
+  });
+}
+
+fetchList();
+
+function createCard(post) {
+  console.log("createCard triggered");
+  console.log("post", post);
+  var title = post.title,
+      body = post.body;
+  var cardElt = document.createElement("div");
+  cardElt.classList.add("card");
+  var imageElt = document.createElement("img");
+  imageElt.classList.add("image");
+  imageElt.src = "https://picsum.photos/300/200";
+  var topElt = document.createElement("div");
+  topElt.classList.add("top");
+  topElt.appendChild(imageElt);
+  var titleElt = document.createElement("div");
+  titleElt.classList.add("title");
+  console.log("title", title);
+  titleElt.innerText = title.split(" ")[0];
+  var descriptionElt = document.createElement("div");
+  descriptionElt.classList.add("description");
+  descriptionElt.innerText = body;
+  var tagElt = document.createElement("div");
+  tagElt.classList.add("tag");
+  var tags = Array(2).fill(tagElt).forEach(function (element, index) {
+    element.textContent = title.split(" ")[2 + index];
+  });
+  var tagsElt = document.createElement("div");
+  tagsElt.classList.add("tags");
+  var bottomElt = document.createElement("div");
+  bottomElt.classList.add("bottom");
+  bottomElt.appendChild(titleElt);
+  bottomElt.appendChild(descriptionElt); // bottomElt.appendChild(tagsElt);
+
+  console.log("bottomElt", bottomElt);
+  cardElt.appendChild(topElt);
+  cardElt.appendChild(bottomElt);
+  var cardContainer = document.getElementById("cards-container");
+  cardContainer.appendChild(cardElt);
+  console.log("cardContainer", cardContainer);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,67 @@
 const message = "Write your JavaScript in src/main.js";
 console.log(message);
+
+function fetchList() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res);
+      createCard(res[0]);
+    });
+}
+
+fetchList();
+
+function createCard(post) {
+  console.log("createCard triggered");
+  console.log("post", post);
+  const { title, body } = post;
+  const cardElt = document.createElement("div");
+  cardElt.classList.add("card");
+
+  const imageElt = document.createElement("img");
+  imageElt.classList.add("image");
+  imageElt.src = "https://picsum.photos/300/200";
+
+  const topElt = document.createElement("div");
+  topElt.classList.add("top");
+  topElt.appendChild(imageElt);
+
+  const titleElt = document.createElement("div");
+  titleElt.classList.add("title");
+  console.log("title", title);
+
+  titleElt.innerText = title.split(" ")[0];
+
+  const descriptionElt = document.createElement("div");
+  descriptionElt.classList.add("description");
+  descriptionElt.innerText = body;
+
+  const tagElt = document.createElement("div");
+  tagElt.classList.add("tag");
+
+  const tags = Array(2)
+    .fill(tagElt)
+    .forEach(function (element, index) {
+      element.textContent = title.split(" ")[2 + index];
+    });
+
+  const tagsElt = document.createElement("div");
+  tagsElt.classList.add("tags");
+
+  const bottomElt = document.createElement("div");
+  bottomElt.classList.add("bottom");
+
+  bottomElt.appendChild(titleElt);
+  bottomElt.appendChild(descriptionElt);
+  // bottomElt.appendChild(tagsElt);
+
+  console.log("bottomElt", bottomElt);
+
+  cardElt.appendChild(topElt);
+  cardElt.appendChild(bottomElt);
+
+  const cardContainer = document.getElementById("cards-container");
+  cardContainer.appendChild(cardElt);
+  console.log("cardContainer", cardContainer);
+}
